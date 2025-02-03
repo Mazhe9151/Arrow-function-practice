68e6702e133737242d623cb227bbea593ac3d9a6
Mazhe9151 <matthewz61@nycstudents.net>
2025-02-03 16:29:20 +0000

    yes

--- LAB1/practice/script.js
+++ LAB1/practice/script.js
@@ -1,16 +1,57 @@
 // List 4 data types in a multiline comment
-
+/*
+Integer
+String
+Boolean
+Float
+*/
 // Typeof
+console.log(typeof 42);
+console.log(typeof "blue");
+console.log(typeof false);
 
+var x = 1;
+var thing = "red";
+var truth = true;
+console.log(typeof x);
+console.log(typeof thing);
+console.log(typeof truth);
 // Let vs Const vs Var
+var x = 2; 
 
+let age = 15;
+age = 16;
+
+const name = "Joe";
+alert(name);
 // Reassignment (no let)
+age = 10;
+age = 23;
+age = 25;
 
+alert(age);
 // Essential Operations
-
+let sum = age + 2;
+console.log(sum);
+sum = sum - 5;
+console.log(sum);
+sum = sum / 2;
+console.log(sum);
+sum = sum + 10;
+console.log(sum);
+sum += 10
+console.log(sum);
+sum -= 4
+console.log(sum);
+sum *= 5;
+console.log(sum);
 // Additional operations
 
 // User Input
+const subject = prompt("What is your favorite subject?");
+console.log("Your favorite subject is " + subject);
+console.log("Your favorite subject is ", subject);
+console.log(`Your favorite subject is ${subject}`);
 
 // Concatenation v. templates
 
